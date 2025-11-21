9ea8d4d108bfd386ae950372669843724d69399d
sheyxov <cavansirsheyxov@gmail.com>
2025-11-21 19:16:10 +0000

    task

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,10 +2,14 @@
 """
 This module defines the Square class used to represent a geometric square.
 """
+
+
 class Square:
     """
     This class defines a geometric square with a private size attribute.
     """
+
+
     def __init__(self, size):
         """
         Initialize a new Square instance with the given size.
